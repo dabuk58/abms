8524907de48a2fb259b195d8418e8922d6a3a3a3
dabuk58 <kubad58@onet.pl>
2024-11-12 20:28:36 +0100

    Routing, header and home draft

--- frontend/ABMS/src/app/app.routes.ts
+++ frontend/ABMS/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { ROUTES } from './core/constants/routes-constants';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: `/${ROUTES.HOME}`,
+    pathMatch: 'full',
+  },
+  {
+    path: ROUTES.HOME,
+    loadComponent: () =>
+      import('./features/home/pages/home-page/home-page.component').then(
+        (c) => c.HomePageComponent
+      ),
+  },
+];
