9940e0877ff57b06a4e80d76c65c30ea52fe0d38
dabuk58 <kubad58@onet.pl>
2024-11-13 00:34:45 +0100

    Added primeng and translations library

--- frontend/ABMS/src/app/app.component.ts
+++ frontend/ABMS/src/app/app.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  TranslateDirective,
+  TranslatePipe,
+  TranslateService,
+} from '@ngx-translate/core';
 import { HeaderComponent } from './core/components/header/header.component';
+import translationsPL from './../../public/i18n/pl.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['pl']);
+    this.translate.setTranslation('pl', translationsPL);
+    this.translate.setDefaultLang('pl');
+    this.translate.use('pl');
+  }
+}
--- frontend/ABMS/src/app/features/home/pages/home-page/home-page.component.ts
+++ frontend/ABMS/src/app/features/home/pages/home-page/home-page.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, TranslatePipe, TranslateDirective],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
