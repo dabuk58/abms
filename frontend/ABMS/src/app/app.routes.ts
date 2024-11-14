377f01deed4a66215f54a2f78256ae796e69e15d
dabuk58 <kubad58@onet.pl>
2024-11-14 21:12:56 +0100

    Profile page with guard

--- frontend/ABMS/src/app/app.routes.ts
+++ frontend/ABMS/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
 import { ROUTES } from './core/constants/routes-constants';
 
 export const routes: Routes = [
@@ -13,5 +14,17 @@ export const routes: Routes = [
       import('./features/home/pages/home-page/home-page.component').then(
         (c) => c.HomePageComponent
       ),
+  },
+  {
+    path: ROUTES.PROFILE,
+    loadComponent: () =>
+      import(
+        './features/profile/pages/profile-page/profile-page.component'
+      ).then((c) => c.ProfilePageComponent),
+    canActivate: [MsalGuard],
+  },
+  {
+    path: '**',
+    redirectTo: ROUTES.HOME,
   },
 ];
