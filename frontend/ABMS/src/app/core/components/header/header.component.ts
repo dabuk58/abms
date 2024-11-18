e705b3256913e13c25596444d43e0cdf65ff956d
dabuk58 <kubad58@onet.pl>
2024-11-18 17:55:03 +0100

    Header animation on scroll

--- frontend/ABMS/src/app/core/components/header/header.component.ts
+++ frontend/ABMS/src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { MenuItem } from 'primeng/api';
@@ -23,6 +24,7 @@ import { LoginModalComponent } from '../login-modal/login-modal.component';
     RouterLink,
     DynamicDialogModule,
     SplitButtonModule,
+    NgClass,
   ],
   providers: [DialogService],
   templateUrl: './header.component.html',
@@ -32,6 +34,8 @@ export class HeaderComponent {
   ROUTES = ROUTES;
   splitButtonConfig: MenuItem[];
   loginDialogRef?: DynamicDialogRef;
+  headerCollapsed = false;
+  private previousScrollTop = 0;
 
   constructor(
     public dialogService: DialogService,
@@ -64,5 +68,20 @@ export class HeaderComponent {
 
   onProfile(): void {
     this.router.navigate([ROUTES.PROFILE]);
+  }
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    const currentScrollTop =
+      window.scrollY || document.documentElement.scrollTop;
+
+    if (currentScrollTop > this.previousScrollTop) {
+      this.headerCollapsed = true;
+    }
+    if (currentScrollTop == 0) {
+      this.headerCollapsed = false;
+    }
+
+    this.previousScrollTop = currentScrollTop;
   }
 }
